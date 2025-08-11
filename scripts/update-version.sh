b41d47726bfaf24d6eb2b2d246bd254c6cb107fc
Anil Kulkarni <akulkarni@anaconda.com>
2025-08-11 16:55:57 -0700

    Fix the update-version script to be mac-compatible

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -17,7 +17,11 @@ echo "Updating version to $NEW_VERSION in all package.json files..."
 
 # Update root package.json
 echo "Updating root package.json..."
-sed -i "s/\"version\": \"[^\"]*\"/\"version\": \"$NEW_VERSION\"/" package.json
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "s/\"version\": \"[^\"]*\"/\"version\": \"$NEW_VERSION\"/" package.json
+else
+    sed -i "s/\"version\": \"[^\"]*\"/\"version\": \"$NEW_VERSION\"/" package.json
+fi
 
 # Update all packages/*/package.json files
 for pkg in packages/*/package.json; do
@@ -25,10 +29,13 @@ for pkg in packages/*/package.json; do
         echo "Updating $pkg..."
         
         # Update version field
-        sed -i "s/\"version\": \"[^\"]*\"/\"version\": \"$NEW_VERSION\"/" "$pkg"
-        
-        # Update @japikey/* dependencies to exact version
-        sed -i "s/\"@japikey\/\([^\"]*\)\": \"[^\"]*\"/\"@japikey\/\1\": \"$NEW_VERSION\"/" "$pkg"
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+            sed -i '' "s/\"version\": \"[^\"]*\"/\"version\": \"$NEW_VERSION\"/" "$pkg"
+            sed -i '' "s/\"@japikey\/\([^\"]*\)\": \"[^\"]*\"/\"@japikey\/\1\": \"$NEW_VERSION\"/" "$pkg"
+        else
+            sed -i "s/\"version\": \"[^\"]*\"/\"version\": \"$NEW_VERSION\"/" "$pkg"
+            sed -i "s/\"@japikey\/\([^\"]*\)\": \"[^\"]*\"/\"@japikey\/\1\": \"$NEW_VERSION\"/" "$pkg"
+        fi
     fi
 done
 
